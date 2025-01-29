cccf2e0ad2e7fff563b4e4394079d7fceb1b970c
Cod1ngcase <codingcase95@gmail.com>
2025-01-29 06:53:22 -0700

    Change Date Format java
    changed date format to mmm instead of mm

--- script.js
+++ script.js
@@ -11,10 +11,15 @@ function calculateDate() {
     let date = new Date(cruiseDate);
     date.setDate(date.getDate() - parseInt(cruiseLength));
 
-    let formattedDate = (date.getMonth() + 1).toString().padStart(2, '0') + "/" +
+    // Array of abbreviated month names
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    
+    // Format date as MMM/DD/YYYY
+    let formattedDate = monthNames[date.getMonth()] + "/" +
                         date.getDate().toString().padStart(2, '0') + "/" +
                         date.getFullYear();
     
     resultDiv.innerHTML = "<strong>Final Payment Due:</strong> " + formattedDate;
     resultDiv.style.display = "block";
 }
+
